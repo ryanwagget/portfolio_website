a191714e2711a3b2e907eb0fc6df397abc7cce20
Ryan Wagget <ryanwagget@ryans-mbp.fc.ca>
2017-10-16 17:43:26 -0400

    Started javascript, fixed code

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
 $(document).foundation()
-//Custom javascript goes here
+
+//variables
+var tree = document.querySelector("#tree");
+var dog = document.querySelector("#dog");
+var dogTop;
+var screenPos;
+//Functions
+
+
+function colorChange ()
+{
+  screenPos = window.scrollY;
+  dogTop = dog.offsetTop;
+    if(dog.scrollTop == 0)
+    {
+      console.log("yay");
+    }
+
+}
+
+//listeners
+window.addEventListener("scroll", colorChange, false);
