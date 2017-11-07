fc8feaacc69f1946e059be678f55532b2a39474d
Ryan Wagget <ryanwagget@Ryans-MacBook-Pro.local>
2017-11-07 15:29:05 -0500

    html,css,js complete

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ var color = document.querySelector("#tree");
 var screenHeight = window.innerHeight; //height of browser window
 var treeOpac = document.querySelector("#tree").style.opacity; //opacity of tree image
 var nav = document.querySelector("#mainNav");
-
+var links = nav.querySelectorAll("a");
 //Functions
 
-/*if (window.scrollY + 10) //when distance between origin and screen position increases, tree opacity decreases
-{
-  console.log("working");
-  //treeOpac - 1;
-  TweenMax.to(color, 2, {opacity: 0});
-}*/
+function scrollit(evt){
+  //console.log(evt.target.id);
+  var idNum = evt.target.id;
+  console.log("section"+idNum);
+  TweenLite.to(window, 1, {scrollTo:{y:"#section"+idNum, offsetY:20}});
+}
 
 function fadeIn()
 {
@@ -28,5 +28,18 @@ function colorChange()
 }
 
 //listeners
-window.addEventListener("scroll", colorChange, false);
 window.addEventListener("load", fadeIn, false);
+for(var i=0; i<links.length; i++){
+  links[i].addEventListener("click", scrollit, false);
+}
+
+
+
+
+//window.addEventListener("scroll", colorChange, false);
+/*if (window.scrollY + 10) //when distance between origin and screen position increases, tree opacity decreases
+{
+  console.log("working");
+  //treeOpac - 1;
+  TweenMax.to(color, 2, {opacity: 0});
+}*/
